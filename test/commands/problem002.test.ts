2d734fe8dbec8b5f17ce48bc1d74edb21f11df8a
Michael Steichen <michael.steichen@gmail.com>
2020-11-23 20:27:11 -0600

    adds problem 2 solution + tests

---
+++ test/commands/problem002.test.ts
@@ -0,0 +1,24 @@
+import {expect, test} from '@oclif/test'
+
+describe('problem002', () => {
+  test
+  .stdout()
+  .command(['problem002', '25'])
+  .it('runs problem002 25', ctx => {
+    expect(ctx.stdout).to.contain('10')
+  })
+
+  test
+  .stdout()
+  .command(['problem002', '100'])
+  .it('runs problem002 100', ctx => {
+    expect(ctx.stdout).to.contain('44')
+  })
+
+  test
+  .stdout()
+  .command(['problem002', '4000000'])
+  .it('runs problem002 4000000', ctx => {
+    expect(ctx.stdout).to.contain('23')
+  })
+})
